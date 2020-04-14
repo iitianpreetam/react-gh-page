9f4a788f6c2e42794eb0f2b0c54cdd42bd936c0d
Preetam Raj <iitianpreetam@gmail.com>
2020-04-14 19:47:06 +0530

    Updates

---
+++ precache-manifest.3942fe7347aaa0d38c95f4787fc97ad8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ac9f80a0321c22868db6b123dc938c",
+    "url": "/react-gh-page/index.html"
+  },
+  {
+    "revision": "b18b942d2a1299f539bc",
+    "url": "/react-gh-page/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "6a81b93325d7c7b0e003",
+    "url": "/react-gh-page/static/css/main.c88451cc.chunk.css"
+  },
+  {
+    "revision": "b18b942d2a1299f539bc",
+    "url": "/react-gh-page/static/js/2.27dd4f24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-page/static/js/2.27dd4f24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a81b93325d7c7b0e003",
+    "url": "/react-gh-page/static/js/main.0d2e6a5a.chunk.js"
+  },
+  {
+    "revision": "4f4b4ceb160818deed99",
+    "url": "/react-gh-page/static/js/runtime-main.f23921dc.js"
+  }
+]);
