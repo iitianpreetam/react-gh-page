3080f04643c27b34100aef177f048a9fd523d0a2
Preetam Raj <iitianpreetam@gmail.com>
2020-04-14 19:43:45 +0530

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+import 'tachyons';
+
+function Header() {
+  return (
+    <h1 className="header tc">How to deploy react app to github page</h1>
+  );
+}
+
+export default Header;
